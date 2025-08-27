a347f5e036851f7f7ebab2b64894f0b581f21bb6
Hyodong Kim <gyehd1230@gmail.com>
2025-08-27 14:35:18 +0900

    공통 -  API 응답을 표준화하는 공통 응답 래퍼 클래스

--- src/main/java/com/server/KH_StudyProjects_WeatherServer/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/server/KH_StudyProjects_WeatherServer/common/exception/GlobalExceptionHandler.java
@@ -1,6 +1,6 @@
 package com.server.KH_StudyProjects_WeatherServer.common.exception;
 
-import com.server.KH_StudyProjects_WeatherServer.common.dto.ApiResponse;
+import com.server.KH_StudyProjects_WeatherServer.common.util.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
